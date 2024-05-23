c1e07b7c0ee3e2081c368321150cdea8cf09a087
algorathem <kcleck06@gmail.com>
2024-05-23 11:01:27 +0800

    animate card

--- Assets/Scripts/PersistenceScript.cs
+++ Assets/Scripts/PersistenceScript.cs
@@ -20,22 +20,26 @@ public class PersistenceScript : MonoBehaviour
         if (winText == "Hamster")
         {
             GameObject.FindGameObjectWithTag("Hamster").GetComponent<SpriteRenderer>().enabled = true;
-         
+            GameObject.FindGameObjectWithTag("Hamster").GetComponent<BoxCollider2D>().enabled = true;
+
         }
         else if (winText == "Cat")
         {
             GameObject.FindGameObjectWithTag("Cat").GetComponent<SpriteRenderer>().enabled = true;
-         
+            GameObject.FindGameObjectWithTag("Cat").GetComponent<BoxCollider2D>().enabled = true;
+
         }
         else if (winText == "Dog")
         {
             GameObject.FindGameObjectWithTag("Dog").GetComponent<SpriteRenderer>().enabled = true;
-          
+            GameObject.FindGameObjectWithTag("Dog").GetComponent<BoxCollider2D>().enabled = true;
+
         }
         else if (winText == "Rabbit")
         {
             GameObject.FindGameObjectWithTag("Rabbit").GetComponent<SpriteRenderer>().enabled = true;
-          
+            GameObject.FindGameObjectWithTag("Rabbit").GetComponent<BoxCollider2D>().enabled = true;
+
         }
 
 
