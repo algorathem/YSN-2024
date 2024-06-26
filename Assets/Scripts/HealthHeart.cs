03931043fd38bcd16f886570a16ece6801552b12
algorathem <kcleck06@gmail.com>
2024-06-26 22:15:10 +0800

    changed heart script

---
+++ Assets/Scripts/HealthHeart.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class HealthHeart : MonoBehaviour
+{
+    public Sprite emptyHeart, oneQHeart, halfHeart, threeQHeart, fulLHeart;
+    Image heartImage;
+
+    private void Awake()
+    {
+        heartImage = GetComponent<Image>();
+    }
+
+    public void SetHeartImage(HeartStatus status)
+    {
+        switch (status)
+        {
+            case HeartStatus.Empty:
+                heartImage.sprite = emptyHeart;
+                break;
+            case HeartStatus.OneQ:
+                heartImage.sprite = oneQHeart;
+                break;
+            case HeartStatus.Half:
+                heartImage.sprite = halfHeart;
+                break;
+            case HeartStatus.Full:
+                heartImage.sprite = fulLHeart;
+                break;
+        }
+    }
+    public enum HeartStatus
+    {
+        Empty = 0,
+        OneQ = 1,
+        Half = 2,
+        ThreeQ = 3,
+        Full = 4
+
+    }
+    
+
+
+
+
+}
+
+
