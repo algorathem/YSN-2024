e196680243c9bb53977f4e1a954ea8c1a37b8189
algorathem <kcleck06@gmail.com>
2024-06-28 09:33:47 +0800

    connect heart UI to health point values

--- Assets/Scripts/HealthManager.cs
+++ Assets/Scripts/HealthManager.cs
@@ -9,22 +9,33 @@ public class HealthManager : MonoBehaviour
 
     private void Start()
     {
-        DrawHeart();
-    }
-
-    public void DrawHeart()
-    {
         CreateEmptyHeart();
     }
+
+    //public void DrawHeart()
+    //{
+    //    CreateEmptyHeart();
+    //    for (int i = 0; i < 1; i++)
+    //    {
+    //        int heartStatusRemainder = (int)Mathf.Clamp(health - (i * 2), 0, 4);
+    //        heartComponent.SetHeartImage()
+    //    }
+    //}
     public void CreateEmptyHeart()
     {
         GameObject newHeart = Instantiate(heartPrefab);
-        
+
         newHeart.transform.SetParent(transform, false);
         //newHeart.transform.localScale = new Vector3(1, 1, 1);
 
         HealthHeart heartComponent = newHeart.GetComponent<HealthHeart>();
         heartComponent.SetHeartImage(HealthHeart.HeartStatus.Full);
 
+        for (int i = 0; i < 1; i++)
+        {
+            int heartStatusRemainder = (int)Mathf.Clamp(health - (i * 2), 0, 4);
+            heartComponent.SetHeartImage((HealthHeart.HeartStatus)(heartStatusRemainder));
+        }
     }
 }
+   
